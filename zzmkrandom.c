00c74a36fa69429dc6857d767729aaa4dbebdb4e
Per Inge Mathisen <per.mathisen@gmail.com>
2010-06-22 15:12:02 +0200

    Two new experimental programs in progress - zztojpegls (convert uncompressed to jpeg-ls) and zzmkrandom (generate pseudo-random unit test DICOM files). Neither tool is currently complete or usable.

---
+++ zzmkrandom.c
@@ -0,0 +1,176 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define UID_SecondaryCaptureImageStorage "1.2.840.10008.5.1.4.1.1.7"
+#define UID_LittleEndianExplicitTransferSyntax  "1.2.840.10008.1.2.1"
+#define UNLIMITED 0xffffffff
+
+enum syntax
+{
+	IMPLICIT, EXPLICIT
+};
+
+static enum syntax syntax = EXPLICIT;
+
+static void implicit(FILE *fp, uint16_t group, uint16_t element, uint32_t length)
+{
+	fwrite(&group, 2, 1, fp);
+	fwrite(&element, 2, 1, fp);
+	fwrite(&length, 4, 1, fp);
+}
+
+static void explicit1(FILE *fp, uint16_t group, uint16_t element, const char *vr, uint16_t length)
+{
+	fwrite(&group, 2, 1, fp);
+	fwrite(&element, 2, 1, fp);
+	fwrite(&vr[0], 1, 1, fp);
+	fwrite(&vr[1], 1, 1, fp);
+	fwrite(&length, 2, 1, fp);
+}
+
+static void explicit2(FILE *fp, uint16_t group, uint16_t element, const char *vr, uint32_t length)
+{
+	uint16_t zero = 0;
+
+	fwrite(&group, 2, 1, fp);
+	fwrite(&element, 2, 1, fp);
+	fwrite(&vr[0], 1, 1, fp);
+	fwrite(&vr[1], 1, 1, fp);
+	fwrite(&zero, 2, 1, fp);
+	fwrite(&length, 4, 1, fp);
+}
+
+static void wUL(FILE *fp, uint16_t group, uint16_t element, uint32_t value)
+{
+	if (syntax == EXPLICIT) explicit1(fp, group, element, "UL", sizeof(value));
+	else implicit(fp, group, element, sizeof(value));
+	fwrite(&value, sizeof(value), 1, fp);
+}
+
+static void wOB(FILE *fp, uint16_t group, uint16_t element, const char *string, int length)
+{
+	int wlen = length;
+	if (length % 2 != 0) wlen++;			// padding
+	if (syntax == EXPLICIT) explicit2(fp, group, element, "OB", length);
+	else implicit(fp, group, element, length);
+	fwrite(string, 1, length, fp);
+	if (length % 2 != 0) fwrite("", 1, 1, fp);	// pad
+}
+
+static void wUI(FILE *fp, uint16_t group, uint16_t element, const char *string)
+{
+	int length = strlen(string) + 1;
+	int wlen = length;
+
+	if (length % 2 != 0) wlen++;			// padding
+	if (syntax == EXPLICIT) explicit1(fp, group, element, "UI", wlen);
+	else implicit(fp, group, element, wlen);
+	fwrite(string, 1, length, fp);
+	if (length % 2 != 0) fwrite("", 1, 1, fp);	// pad
+}
+
+static void explicitheader(FILE *fp, const char *sopclass, const char *transfer)
+{
+	char version[3];
+	const char *dicm = "DICM";
+	char *zeroes = malloc(128);
+	uint32_t size;
+
+	version[0] = 0;
+	version[1] = 1;
+	version[2] = 0;
+	memset(zeroes, 0, sizeof(zeroes));
+
+	fwrite(zeroes, 1, 128, fp);
+	fwrite(dicm, 1, 4, fp);
+
+	wUL(fp, 0x0002, 0x0000, 0);	// length zero, fixing it below
+	wOB(fp, 0x0002, 0x0001, version, 2);
+	wUI(fp, 0x0002, 0x0002, sopclass);
+	wUI(fp, 0x0002, 0x0003, "1.2.3.4.0");
+	wUI(fp, 0x0002, 0x0010, transfer);
+	wUI(fp, 0x0002, 0x0012, "1.2.3.4.8.2");
+//	wSH(fp, 0x0002, 0x0013, "TEST");
+//	wAE(fp, 0x0002, 0x0016, "TEST");
+
+	// write group size
+	size = ftell(fp);
+	fseek(fp, 128+4+8, SEEK_SET);
+	fwrite(&size, 4, 1, fp);	// set size
+	fseek(fp, 0, SEEK_END);		// return position
+}
+
+static void explicitgeneric(FILE *fp, const char *sopclass)
+{
+	wUI(fp, 0x0008, 0x0016, sopclass);
+	wUI(fp, 0x0008, 0x0018, "1.2.3.4.0");
+
+	explicit1(fp, 0x0008, 0x0020, "DA", 0);
+	explicit1(fp, 0x0008, 0x0030, "TM", 0);
+	explicit1(fp, 0x0008, 0x0050, "SH", 0);
+	explicit1(fp, 0x0008, 0x0090, "PN", 0);
+	explicit1(fp, 0x0010, 0x0010, "PN", 0);
+	explicit1(fp, 0x0010, 0x0020, "LO", 0);
+	explicit1(fp, 0x0010, 0x0030, "DA", 0);
+	explicit1(fp, 0x0010, 0x0040, "CS", 0);
+	wUI(fp, 0x0020, 0x000d, "1.2.3.4.1");
+	wUI(fp, 0x0020, 0x000e, "1.2.3.4.2");
+	explicit1(fp, 0x0020, 0x0010, "SH", 0);
+	explicit1(fp, 0x0020, 0x0011, "IS", 0);
+	explicit1(fp, 0x0020, 0x0013, "IS", 0);
+	explicit1(fp, 0x0020, 0x0060, "CS", 0);
+}
+
+static void garbfill(FILE *fp)
+{
+	wUL(fp, 0x0028, 0x9001, 1);
+	wUL(fp, 0x0028, 0x9002, 2);
+}
+
+int main(int argc, char **argv)
+{
+	FILE *fp = fopen("tough.dcm", "w");
+
+	(void)argc;
+	(void)argv;
+
+	srand(1000);
+
+	explicitheader(fp, UID_SecondaryCaptureImageStorage, UID_LittleEndianExplicitTransferSyntax);
+	explicitgeneric(fp, UID_SecondaryCaptureImageStorage);
+
+	explicit2(fp, 0x0020, 0x1115, "SQ", UNLIMITED);
+	implicit(fp, 0xfffe, 0xe000, UNLIMITED);
+	garbfill(fp);
+	implicit(fp, 0xfffe, 0xe00d, 0);
+	implicit(fp, 0xfffe, 0xe0dd, 0);
+
+	explicit2(fp, 0x0021, 0x0010, "UN", UNLIMITED);
+	syntax = IMPLICIT;
+	implicit(fp, 0xfffe, 0xe000, UNLIMITED);
+	garbfill(fp);
+	implicit(fp, 0xfffe, 0xe00d, 0);
+	implicit(fp, 0xfffe, 0xe0dd, 0);
+	syntax = EXPLICIT;
+
+	fclose(fp);
+
+	return 0;
+}
+
+/*
+Error - Missing attribute Type 1 Required Element=<ConversionType> Module=<SCEquipment>
+Error - Missing attribute Type 2C Conditional Element=<PatientOrientation> Module=<GeneralImage>
+Error - Missing attribute Type 1 Required Element=<SamplesPerPixel> Module=<ImagePixelMacro>
+Error - Missing attribute Type 1 Required Element=<PhotometricInterpretation> Module=<ImagePixelMacro>
+Error - Missing attribute Type 1 Required Element=<Rows> Module=<ImagePixelMacro>
+Error - Missing attribute Type 1 Required Element=<Columns> Module=<ImagePixelMacro>
+Error - Missing attribute Type 1 Required Element=<BitsAllocated> Module=<ImagePixelMacro>
+Error - Missing attribute Type 1 Required Element=<BitsStored> Module=<ImagePixelMacro>
+Error - Missing attribute Type 1 Required Element=<HighBit> Module=<ImagePixelMacro>
+Error - Missing attribute Type 1 Required Element=<PixelRepresentation> Module=<ImagePixelMacro>
+Error - Missing attribute Type 1C Conditional Element=<PixelData> Module=<ImagePixelMacro>
+*/
---
+++ zztojpegls.c
@@ -0,0 +1,159 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+#include <time.h>
+#include <stdbool.h>
+#include <errno.h>
+#include <limits.h>
+
+#include "charlsintf.h"	// private port of CharLS pseudo-C interface to real C interface
+
+#include "zz_priv.h"
+#include "zzsql.h"
+
+extern bool verbose;	// TODO, fix ugly global
+
+static bool jpegtols(char *filename)
+{
+	struct JlsParamaters params;
+	enum JLS_ERROR err;
+	struct zzfile *zz = zzopen(filename, "r");
+	struct stat st;
+	size_t result;
+	uint16_t group, element;
+	char *src, *dst;
+	uint32_t len, pos, size;
+	char newname[PATH_MAX], *cptr;
+	FILE *fp;
+
+	if (!zz)
+	{
+		fprintf(stderr, "Failed to open file %s", filename);
+		exit(-1);
+	}
+	fstat(fileno(zz->fp), &st);
+	size = st.st_size;
+	strcpy(newname, zz->fullPath);
+	cptr = strrchr(newname, '.');
+	if (cptr && cptr > strrchr(newname, '/')) *cptr = '\0';	// chop off extension, looks weird otherwise; make sure we don't clobber path
+	if (strlen(newname) > PATH_MAX - 4)
+	{
+		fprintf(stderr, "%s - filename too long\n", newname);
+		return false;
+	}
+	strcat(newname, "-ls");
+	fp = fopen(newname, "w");
+	if (!fp)
+	{
+		fprintf(stderr, "%s - could not create out file: %s\n", newname, strerror(errno));
+		return false;
+	}
+
+	memset(&params, 0, sizeof(params));
+	while (!feof(zz->fp) && !ferror(zz->fp))
+	{
+		zzread(zz, &group, &element, &len);
+
+		pos = ftell(zz->fp);
+
+		// Copy (or alter) contents
+
+		// Read out valuable info
+		switch (ZZ_KEY(group, element))
+		{
+		case DCM_BitsStored:
+			break;
+		case DCM_BitsAllocated:
+			params.bitspersample = zzgetuint16(zz);
+			break;
+		case DCM_PhotometricInterpretation:
+			// TODO, CS. need to be "MONOCHROME1", "MONOCHROME2" or "RGB" (or "PALETTE COLOR"?)
+			break;
+		case DCM_PixelRepresentation:
+			// TODO, US; 0 - unsigned, 1 - signed
+			break;
+		case DCM_Rows:
+			params.height = zzgetuint16(zz);
+			break;
+		case DCM_Columns:
+			params.width = zzgetuint16(zz);
+			break;
+		case DCM_SamplesPerPixel:
+			params.components = zzgetuint16(zz);
+			if (params.components == 3)
+			{
+				params.ilv = ILV_SAMPLE; // RGB
+			}
+			else
+			{
+				params.ilv = ILV_NONE; // grayscale
+			}
+			break;
+		case DCM_PlanarConfiguration:
+			if (zzgetuint16(zz) != 0)
+			{
+				fprintf(stderr, "%s unsupported planar configuration\n", filename);
+				return false;
+			}
+			break;
+		case DCM_PixelData:
+			src = malloc(len);
+			dst = malloc(len);	// assume that we will not compress worse
+			result = fread(src, len, 1, zz->fp);
+			if (result != 1)
+			{
+				fprintf(stderr, "%s - bad image length (len %d != result %d)\n", filename, len, (int)result);
+			}
+			//params.colorTransform = 1;
+			//params.allowedlossyerror = 3;
+			params.bytesperline = 0;
+			if (verbose)
+				printf("Encoding %dx%d image with %d bits per sample, %d samples per pixel\n",
+				       params.width, params.height, params.bitspersample, params.components);
+			result = 0;
+			err = JpegLsEncode(dst, len, &result, src, len, &params);
+			switch (err)
+			{
+			case OK: break;
+			case InvalidJlsParameters: fprintf(stderr, "%s - invalid encoding parameters\n", filename); break;
+			case ParameterValueNotSupported: fprintf(stderr, "%s - not supported encoding parameters\n", filename); break;
+			case UncompressedBufferTooSmall: fprintf(stderr, "%s - could not yield compression gain\n", filename); break;
+			case InvalidCompressedData:
+			case ImageTypeNotSupported:
+			case CompressedBufferTooSmall: fprintf(stderr, "%s - this should not happen\n", filename); break;
+			case UnsupportedBitDepthForTransform: fprintf(stderr, "%s - unsupported bit depth\n", filename); break;
+			case UnsupportedColorTransform: fprintf(stderr, "%s - unsupported color transform\n", filename); break;
+			};
+			free(src);
+			free(dst);
+			break;
+		default:
+			break;
+		}
+
+		// Skip ahead
+		if (!feof(zz->fp) && len != 0xFFFFFFFF && len > 0)
+		{
+			fseek(zz->fp, pos + len, SEEK_SET);
+		}
+	}
+
+	fclose(fp);
+	zz = zzclose(zz);
+	return true;
+}
+
+int main(int argc, char **argv)
+{
+	int i;
+
+	for (i = zzutil(argc, argv, 2, "<filenames>", "Convert uncompressed to JPEG-ls compressed DICOM"); i < argc; i++)
+	{
+		jpegtols(argv[i]);
+	}
+
+	return 0;
+}
