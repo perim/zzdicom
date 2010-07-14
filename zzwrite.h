4e69a19c032855fec13c69357108df9e325b8aa8
Per Inge Mathisen <per.mathisen@gmail.com>
2010-07-14 16:57:57 +0200

    Forgot to commit this file

---
+++ zzwrite.h
@@ -0,0 +1,17 @@
+#ifndef ZZ_WRITE_H
+#define ZZ_WRITE_H
+
+#include "zz_priv.h"
+
+void zzwAE(struct zzfile *zz, zzKey key, const char *string);
+void zzwAS(struct zzfile *zz, zzKey key, const char *string);
+void zzwCS(struct zzfile *zz, zzKey key, const char *string);
+void zzwOB(struct zzfile *zz, zzKey key, const char *string, int length);
+void zzwSH(struct zzfile *zz, zzKey key, const char *string);
+void zzwUI(struct zzfile *zz, zzKey key, const char *string);
+void zzwUL(struct zzfile *zz, zzKey key, uint32_t value);
+
+void zzwHeader(struct zzfile *zz, const char *sopclass, const char *transfer);
+void zzwEmpty(struct zzfile *zz, zzKey key, const char *vr);
+
+#endif
