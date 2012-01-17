66bf69a59e49e5d5951ad7d521c431d20cb18073
Per Inge Mathisen <per.mathisen@gmail.com>
2012-01-17 18:51:08 +0100

    Change from smart linear lookup to binary search in part 6 table, giving a nice speed boost of approx 30%. zzdump is very fast, approx 350% faster than gdcmdump and 90% faster than dcmdump...

--- part6.c
+++ part6.c
@@ -44,31 +44,29 @@ const struct privatedic *zzprivtag(uint16_t group, uint16_t element, const char 
 	return NULL;
 }
 
+// binary search of part6 table
 const struct part6 *zztag(uint16_t group, uint16_t element)
 {
-	// Since we read DICOM files sequentially by ascending group/element tags, assuming the same for 
-	// tag lookups much improves lookup speed. Hence the lastidx hack.
-	static int lastidx = 0;
-	const int max = ARRAY_SIZE(part6_table);
-	int i;
-
-	for (i = lastidx; i < max; i++)
+	int middle, fgroup, felement;
+	int high = ARRAY_SIZE(part6_table);
+	int low = 0;
+	while (low <= high)
 	{
-		if (group == part6_table[i].group && element == part6_table[i].element)
+		middle = low + (high - low) / 2;
+		fgroup = part6_table[middle].group;
+		felement = part6_table[middle].element;
+		if (group < fgroup || (group == fgroup && element < felement))
 		{
-			lastidx = i;
-			return &part6_table[i];
+			high = middle - 1;	// lower half
+		}
+		else if (group > fgroup || (group == fgroup && element > felement))
+		{
+			low = middle + 1;  // upper half
+		}
+		else
+		{
+			return &part6_table[middle];
 		}
 	}
-	for (i = 0; i < lastidx; i++)
-	{
-		if (group == part6_table[i].group && element == part6_table[i].element)
-		{
-			lastidx = i;
-			return &part6_table[i];
-		}
-	}
-
 	return NULL;
 }
-
